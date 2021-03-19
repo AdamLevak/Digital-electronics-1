b5e3811c26c20fda996d8024cd80b47cec80635b
AdamLevak <xlevak00@vutbr.cz>
2021-03-19 14:14:03 +0100

    almost done

--- Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
+++ Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
@@ -1,69 +1,14 @@
-## Lab 6
-# Task 1
-- Timing diagram figure for displaying value 3.142
+------------------------------------------------------------------------
+--
+-- Template for 4-digit 7-segment display driver testbench.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020 Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------------------
 
-![graf](Images/1.png)
-
-```javascript
-{
-  signal:
-  [
-    ['Digit position',
-      {name: 'Common anode: AN(3)', wave: 'xx01..01..01'},
-      {name: 'AN(2)', wave: 'xx101..01..0'},
-      {name: 'AN(1)', wave: 'xx1.01..01..'},
-      {name: 'AN(0)', wave: 'xx1..01..01.'},
-    ],
-    ['Seven-segment data',
-      {name: '4-digit value to display', wave: 'xx3333555599', data: ['3','1','4','2','3','1','4','2','3','1']},
-      {name: 'Cathod A: CA', wave: 'xx01.0.1.0.1'},
-      {name: 'CB', wave: 'xx0.........'},
-      {name: 'CC', wave: 'xx0..10..10.'},
-      {name: 'CD', wave: 'xx01.0.1.0.1'},
-      {name: 'CE', wave: 'xx1..01..01.'},
-      {name: 'CF', wave: 'xx1.01..01..'},
-      {name: 'CG', wave: 'xx010..10..1'},
-    ],
-    {name: 'Decimal point: DP', wave: 'xx01..01..01'},
-  ],
-  head:
-  {
-    text: '                    4ms   4ms   4ms   4ms   4ms   4ms   4ms   4ms   4ms   4ms',
-  },
-}
-```
-
-# Task 2
-- Listing of VHDL code of the process p_mux
-```vhdl
- p_mux : process(s_cnt, data0_i, data1_i, data2_i, data3_i, dp_i)
-    begin
-        case s_cnt is
-            when "11" =>
-                s_hex <= data3_i;
-                dp_o  <= dp_i(3);
-                dig_o <= "0111";
-
-            when "10" =>
-                s_hex <= data2_i;
-                dp_o  <= dp_i(2);
-                dig_o <= "1011";
-
-            when "01" =>
-                s_hex <= data1_i;
-                dp_o  <= dp_i(1);
-                dig_o <= "1101";
-
-            when others =>
-                s_hex <= data0_i;
-                dp_o  <= dp_i(0);
-                dig_o <= "1110";
-        end case;
-    end process p_mux;
-```
-
-- Listing of VHDL testbench file tb_driver_7seg_4digits
-```vhdl
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -165,71 +110,3 @@ begin
     end process p_stimulus;
 
 end architecture testbench;
-```
-- Screenshot with simulated time waveforms
-
-![graf](Images/graf.png)
-
-- Listing of VHDL architecture of the top layer.
-
-```vhdl
-architecture Behavioral of top is
- -- No internal signals
-begin
- -- Instance (copy) of driver_7seg_4digits entity
-    driver_seg_4 : entity work.driver_7seg_4digits
-        port map(
-            clk        => CLK100MHZ,
-            reset      => BTNC,
-            data0_i(3) => SW(3),
-            data0_i(2) => SW(2),
-            data0_i(1) => SW(1),
-            data0_i(0) => SW(0),
-          
-            data1_i(3) => SW(7),
-            data1_i(2) => SW(6),
-            data1_i(1) => SW(5),
-            data1_i(0) => SW(4),
-            
-            data2_i(3) => SW(11),
-            data2_i(2) => SW(10),
-            data2_i(1) => SW(9),
-            data2_i(0) => SW(8),
-            
-            data3_i(3) => SW(15),
-            data3_i(2) => SW(14),
-            data3_i(1) => SW(13),
-            data3_i(0) => SW(12),
-            dp_i => "0111",
-            
-            dig_o => AN(4-1 downto 0),
-           
-           seg_o(0) => CA,
-           seg_o(1) => CB,
-           seg_o(2) => CC,
-           seg_o(3) => CD,
-           seg_o(4) => CE,
-           seg_o(5) => CF,
-           seg_o(4) => CG,
-           
-           
-            
-            dp_o => DP
-        );
-
-    -- Disconnect the top four digits of the 7-segment display
-    AN(7 downto 4) <= b"1111";
-
-end architecture Behavioral;
-```
-
-# Task 3
-
-
-
-
-
-
-
-
-
